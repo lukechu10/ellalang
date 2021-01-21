02b7007cf52c79bcc4a1c8717dbfe2057fdd825f
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-01-21 09:04:35 -0800

    fix: accept empty program

--- ella-parser/src/parser.rs
+++ ella-parser/src/parser.rs
@@ -44,11 +44,8 @@ impl<'a> Parser<'a> {
         let lo = self.node_start();
 
         let mut body = Vec::new();
-        loop {
+        while !matches!(self.current_token, Token::Eof | Token::Error) {
             body.push(self.parse_declaration());
-            if self.current_token == Token::Eof {
-                break;
-            }
         }
 
         StmtKind::FnDeclaration {
